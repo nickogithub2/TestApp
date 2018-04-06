2221052eaaa8efe292dbe0bcc50c88c2a37b974f
nickogithub <nickschool45@gmail.com>
2018-04-06 05:31:44 +0200

    Add Client class

---
+++ DemoApplication/src/main/resources/org/app/entities/Client.java
@@ -0,0 +1,13 @@
+package org.app.entities;
+
+public class Client {
+
+	private Long idClient;
+	private String firstName;
+	private String lastName;
+	private String adress;
+	private String telephone;
+	private String email;
+	
+	/*getters and setters*/
+}
